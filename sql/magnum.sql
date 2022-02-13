4d69085882cf75feba276606719d8a10d3706821
grtcdr <ba.tahaaziz@gmail.com>
2022-02-14 00:51:12 +0100

    include sql script to install Users/Flags/History tables

---
+++ sql/magnum.sql
@@ -0,0 +1,135 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Feb 13, 2022 at 09:44 PM
+-- Server version: 10.6.5-MariaDB
+-- PHP Version: 8.1.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `magnum`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Flags`
+--
+
+CREATE TABLE `Flags` (
+  `ID` int(11) NOT NULL,
+  `userID` int(11) NOT NULL,
+  `offense` enum('Harassment','Spam','Violence') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` varchar(400) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `time` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `History`
+--
+
+CREATE TABLE `History` (
+  `ID` int(11) NOT NULL,
+  `userID` int(11) NOT NULL,
+  `activity` enum('Profile','Security','Billing','') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `time` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Users`
+--
+
+CREATE TABLE `Users` (
+  `ID` int(11) NOT NULL,
+  `username` varchar(40) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(80) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `firstName` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `lastName` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `biography` varchar(250) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `avatar` varchar(120) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `status` enum('Active','Disabled','Banned','') COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `Flags`
+--
+ALTER TABLE `Flags`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `fk_userID_flags` (`userID`);
+
+--
+-- Indexes for table `History`
+--
+ALTER TABLE `History`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `fk_userID_hist` (`userID`);
+
+--
+-- Indexes for table `Users`
+--
+ALTER TABLE `Users`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `Flags`
+--
+ALTER TABLE `Flags`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `History`
+--
+ALTER TABLE `History`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT for table `Users`
+--
+ALTER TABLE `Users`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=73;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `Flags`
+--
+ALTER TABLE `Flags`
+  ADD CONSTRAINT `fk_userID_flags` FOREIGN KEY (`userID`) REFERENCES `Users` (`ID`);
+
+--
+-- Constraints for table `History`
+--
+ALTER TABLE `History`
+  ADD CONSTRAINT `fk_userID_hist` FOREIGN KEY (`userID`) REFERENCES `Users` (`ID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
