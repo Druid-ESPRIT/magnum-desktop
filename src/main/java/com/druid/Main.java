9577b850574769dc52907b08d1c6dc898b76af97
grtcdr <ba.tahaaziz@gmail.com>
2022-02-25 03:13:02 +0100

    organize project for fxml support & include the first window!

--- src/main/java/com/druid/Main.java
+++ src/main/java/com/druid/Main.java
@@ -1,7 +1,23 @@
 package com.druid;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
   public static void main(String[] args) {
-    // TODO: Populate entry-point.
+    launch(args);
+  }
+
+  @Override
+  public  void  start(Stage stage) throws IOException {
+    Parent root = FXMLLoader.load(getClass().getResource("/main.fxml"));
+    stage.setTitle("Magnum");
+    stage.setScene(new Scene(root, 300, 200));
+    stage.show();
   }
 }
