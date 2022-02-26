6ff8d03155bdb2697b1a2bed0fd1e0e472b818b1
grtcdr <ba.tahaaziz@gmail.com>
2022-02-26 13:34:55 +0100

    sql: revise the database
    - Add "Administrators" table
    - Add "Podcasters" table
    - Remove unnecessary attributes from "Users" table
    - Reset the sequences of each table back to 1 for auto-incrementable columns.

--- sql/magnum.sql
+++ sql/magnum.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.2
+-- version 4.9.5deb2
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Feb 13, 2022 at 09:44 PM
--- Server version: 10.6.5-MariaDB
--- PHP Version: 8.1.2
+-- Host: localhost:3306
+-- Generation Time: Feb 26, 2022 at 12:30 PM
+-- Server version: 8.0.28-0ubuntu0.20.04.3
+-- PHP Version: 7.4.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -21,7 +22,17 @@ SET time_zone = "+00:00";
 -- Database: `magnum`
 --
 
-USE magnum;
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Administrators`
+--
+
+CREATE TABLE `Administrators` (
+  `ID` int NOT NULL,
+  `firstName` varchar(40) NOT NULL,
+  `lastName` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -30,11 +41,11 @@ USE magnum;
 --
 
 CREATE TABLE `Flags` (
-  `ID` int(11) NOT NULL,
-  `userID` int(11) NOT NULL,
-  `offense` enum('Harassment','Spam','Violence') COLLATE utf8mb4_unicode_ci NOT NULL,
-  `description` varchar(400) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `time` timestamp NOT NULL DEFAULT current_timestamp()
+  `ID` int NOT NULL,
+  `userID` int NOT NULL,
+  `offense` enum('Harassment','Spam','Violence') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` varchar(400) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -44,12 +55,40 @@ CREATE TABLE `Flags` (
 --
 
 CREATE TABLE `History` (
-  `ID` int(11) NOT NULL,
-  `userID` int(11) NOT NULL,
-  `activity` enum('Profile','Security','Billing','') COLLATE utf8mb4_unicode_ci NOT NULL,
-  `description` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `time` timestamp NOT NULL DEFAULT current_timestamp()
+  `ID` int NOT NULL,
+  `userID` int NOT NULL,
+  `activity` enum('Profile','Security','Billing','Core') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Podcasters`
+--
+
+CREATE TABLE `Podcasters` (
+  `ID` int NOT NULL,
+  `firstName` varchar(40) NOT NULL COMMENT 'This attribute can be used as the name of the podcast and not necessarily that of the account holder.',
+  `lastName` varchar(40) NOT NULL COMMENT 'This attribute can be used as the name of the podcast and not necessarily that of the account holder.',
+  `biography` varchar(200) DEFAULT NULL COMMENT 'A short and sweet paragraph that tells users a little bit about the podcaster.',
+  `avatar` varchar(120) DEFAULT NULL COMMENT 'The name and extension of the image file that represents the avatar of the user, e.g. "grtcdr.png"'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Tokens`
+--
+
+CREATE TABLE `Tokens` (
+  `ID` int NOT NULL,
+  `userID` int NOT NULL,
+  `token` varchar(128) NOT NULL,
+  `consumed` tinyint(1) NOT NULL DEFAULT '0',
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -58,20 +97,27 @@ CREATE TABLE `History` (
 --
 
 CREATE TABLE `Users` (
-  `ID` int(11) NOT NULL,
-  `username` varchar(40) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email` varchar(80) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `password` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `firstName` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `lastName` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `biography` varchar(250) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `avatar` varchar(120) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `status` enum('Active','Disabled','Banned','') COLLATE utf8mb4_unicode_ci NOT NULL
+  `ID` int NOT NULL,
+  `username` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(80) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(72) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `firstName` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `lastName` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `biography` varchar(250) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `avatar` varchar(120) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `status` enum('Active','Disabled','Banned','') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `role` enum('Admin','Normal') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'Normal'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `Administrators`
+--
+ALTER TABLE `Administrators`
+  ADD PRIMARY KEY (`ID`);
 
 --
 -- Indexes for table `Flags`
@@ -88,6 +134,19 @@ ALTER TABLE `History`
   ADD KEY `fk_userID_hist` (`userID`);
 
 --
+-- Indexes for table `Podcasters`
+--
+ALTER TABLE `Podcasters`
+  ADD KEY `fk_userID_pod` (`ID`);
+
+--
+-- Indexes for table `Tokens`
+--
+ALTER TABLE `Tokens`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `fk_userID_tokens` (`userID`);
+
+--
 -- Indexes for table `Users`
 --
 ALTER TABLE `Users`
@@ -101,23 +160,35 @@ ALTER TABLE `Users`
 -- AUTO_INCREMENT for table `Flags`
 --
 ALTER TABLE `Flags`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `ID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `History`
 --
 ALTER TABLE `History`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `ID` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Tokens`
+--
+ALTER TABLE `Tokens`
+  MODIFY `ID` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `Users`
 --
 ALTER TABLE `Users`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=73;
+  MODIFY `ID` int NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `Administrators`
+--
+ALTER TABLE `Administrators`
+  ADD CONSTRAINT `fk_userID_admin` FOREIGN KEY (`ID`) REFERENCES `Users` (`ID`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
 --
 -- Constraints for table `Flags`
@@ -130,6 +201,18 @@ ALTER TABLE `Flags`
 --
 ALTER TABLE `History`
   ADD CONSTRAINT `fk_userID_hist` FOREIGN KEY (`userID`) REFERENCES `Users` (`ID`);
+
+--
+-- Constraints for table `Podcasters`
+--
+ALTER TABLE `Podcasters`
+  ADD CONSTRAINT `fk_userID_pod` FOREIGN KEY (`ID`) REFERENCES `Users` (`ID`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `Tokens`
+--
+ALTER TABLE `Tokens`
+  ADD CONSTRAINT `fk_userID_tokens` FOREIGN KEY (`userID`) REFERENCES `Users` (`ID`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
